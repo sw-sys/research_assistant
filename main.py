28323a068f1652ab11270b0e0e4e176b673b742b
sw-sys <sheldonkorpet@gmail.com>
2025-04-12 17:59:40 +0100

    define response data types and add parser

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 # Activate venv with '.\venv\Scripts\activate' for Windows
 # Docs https://python.langchain.com/docs/integrations/providers/anthropic/
+# model summary https://docs.anthropic.com/en/docs/about-claude/models/all-models
 
 import os
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
 
-llm = ChatAnthropic(
-    model='claude-3-5-sonnet-20241022',
-)
+# specify how LLM should respond
 
-response = llm.invoke("Do geese have teeth?")
-print(response)
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
+# define LLM model to respond
+llm = ChatAnthropic(
+    model='claude-3-7-sonnet-20250219',
+)
+# Check we can get response from LLM
+# print(response=llm.invoke("Do geese have teeth?"))
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
